397b3990c62998349b37d786d286eac6f51796dd
joaquinFC <50423169+joaquinFC@users.noreply.github.com>
2021-04-06 18:31:36 -0300

    Update Cliente.java

--- Tp1/tp1/clienteEjercicio3A/src/main/java/Cliente.java
+++ Tp1/tp1/clienteEjercicio3A/src/main/java/Cliente.java
@@ -5,29 +5,26 @@ import java.util.Scanner;
 
 public class Cliente {
      public static void main (String args[]) throws Exception {
-         boolean exit=true;
+     
          int opcion;
          long montoIngresado ;
          ConversorMonetarioRemoto cm = (ConversorMonetarioRemoto)
             Naming.lookup ("rmi://localhost:1099/conversorMonetario");
           
-            
-              
-       
-          
-               System.out.print("ingrese monto : ");
-               Scanner valorEntrada=new Scanner(System.in);
-               montoIngresado=valorEntrada.nextInt();
-            
-               System.out.print("Seleccione una opcion : ");
-               System.out.print("Peso Argentino a Dolar -> 1: ");
-               System.out.print("Dolar a Peso Argentino -> 2 : ");
+               System.out.print("Seleccione una opcion :  \n");
+               System.out.print("Peso Argentino a Dolar -> 1\n ");
+               System.out.print("Dolar a Peso Argentino -> 2 \n ");
                
-              Scanner valorEntrada2=new Scanner(System.in);
-              opcion=valorEntrada2.nextInt();
+                Scanner valorEntrada2=new Scanner(System.in);
+                opcion=valorEntrada2.nextInt();
+              
+              
+               System.out.print("ingrese monto : \n");
+                Scanner valorEntrada=new Scanner(System.in);
+                montoIngresado=valorEntrada.nextInt();
+              
               if(opcion==1)
               {
-                  
                 cm.PesoDolar(montoIngresado);
               }
               else
@@ -35,8 +32,7 @@ public class Cliente {
                  cm.DolarPeso(montoIngresado); 
               }
             
-               
-          
+                         
             System.out.println ("Resultado: " + cm.mostrarResultado());
               
           }
